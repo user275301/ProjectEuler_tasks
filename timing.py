e5507975b089cd901bba5c539d2a4f3306ddab02
MIkhail Sedelnikov <user275301@yandex.ru>
2017-02-19 13:59:11 +0300

    make simple timing function

--- timing.py
+++ timing.py
@@ -7,7 +7,10 @@ if len(sys.argv) != 2:
     print("wrong number of arguments")
     sys.exit(1)
 
+with open(sys.argv[1]) as inputstr:
+    DATA = inputstr.read()
+
 START = time.time()
-exec("sys.argv[1]")
+exec(DATA)
 END = time.time()
 print(START-END)
